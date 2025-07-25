818c3deb68316446314f9b4820b5c1c8a7cdb185
reinis-vimba <reinis.vimba@gmail.com>
2025-07-25 14:31:47 +0300

    Updated config to match github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/CoLab-Hakaton-SigSolar/", // This matches your GitHub repo name
   plugins: [react(), tailwindcss()],
 });
